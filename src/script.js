4e0dc4954fd568664078afcf0e05de944cb794a6
axelquartz <axelarep@gmail.com>
2024-12-09 10:35:20 -0600

    Fetch OMDb movie API and display movie poster in the DOM

--- src/script.js
+++ src/script.js
@@ -3,8 +3,29 @@
 const container = document.querySelector(".container");
 const inputValue = document.getElementById("movie-search");
 const submnitButton = document.getElementById("submit-button");
-//Temporal element
-const displayValue = document.createElement("p");
+const movieTitle = document.createElement("h2");
+const movieDescription = document.createElement("p");
+const posterImage = document.createElement("img");
+
+// API key
+const apiKey = "a28273ae";
+
+// API URL
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
+
+// API Call
+async function getMovie(movieName) {
+  try {
+    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`);
+    const data = await response.json();
+    const poster = data.Poster;
+    posterImage.src = poster;
+    container.appendChild(posterImage);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Event Listeners
 inputValue.addEventListener("input", (e) => {
@@ -14,4 +35,9 @@ inputValue.addEventListener("input", (e) => {
 submnitButton.addEventListener("click", (e) => {
   e.preventDefault();
   container.appendChild(displayValue);
+  const movieName = inputValue.value;
+  inputValue.value = "";
+  console.log(movieName);
+
+  getMovie(movieName);
 });
