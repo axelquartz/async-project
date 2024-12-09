92c34e9301d5b52e64be3bcd1ad057e6a8c0c19f
axelquartz <axelarep@gmail.com>
2024-12-09 00:53:09 -0600

    Define basis

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-alert('Is Working')
+// DOM elements to create
+
+const container = document.querySelector(".container");
+const inputValue = document.getElementById("movie-search");
+const submnitButton = document.getElementById("submit-button");
+//Temporal element
+const displayValue = document.createElement("p");
+
+// Event Listeners
+inputValue.addEventListener("input", (e) => {
+  displayValue.textContent = e.target.value;
+});
+
+submnitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.appendChild(displayValue);
+});
